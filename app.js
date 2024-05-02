1b42c4b3a74ecc828b78a8e3cef863bfe47dc013
Ercan <ercann.6191@gmail.com>
2024-05-03 00:10:05 +0300

    data fetch edildi ve fonksiyon oluşturuldu

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('Dad Jokes Starter');
+const url = 'https://icanhazdadjoke.com/';
+
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click", () => {
+    fetchJoke();
+});
+
+const fetchJoke = async () => {
+    result.textContent = "Loading...";
+    try {
+        const response = await fetch(url, {
+            headers: {
+                Accept: "application/JSON",
+                "User-Agent": "learning app",
+            },
+        });
+        if(!response.ok){
+            throw new Error("errror");
+        }
+        const data = await response.json();
+        result.textContent = data.joke;
+    }catch (error){
+        console.log(error.message);
+        result.textContent = "Something went wrong...";
+    }
+};
+
+fetchJoke
